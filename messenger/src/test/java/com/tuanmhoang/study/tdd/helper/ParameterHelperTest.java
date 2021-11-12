ea9bb40cb934e99400b065c72875e3a314a288c6
Tuan Hoang <hoangminhtuan0505@gmail.com>
2021-11-12 17:39:40 +0700

    [red] getTemplateText_withCliParameterHelper

--- messenger/src/test/java/com/tuanmhoang/study/tdd/helper/ParameterHelperTest.java
+++ messenger/src/test/java/com/tuanmhoang/study/tdd/helper/ParameterHelperTest.java
@@ -3,6 +3,7 @@ package com.tuanmhoang.study.tdd.helper;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParameterHelperTest {
 
@@ -13,6 +14,7 @@ public class ParameterHelperTest {
         parameterHelper = new CliParameterHelper();
         String templateText = parameterHelper.getTemplateText();
         assertNotNull(templateText);
+        assertTrue(templateText.contains("This message is generated with the template in CONSOLE mode"));
     }
 
 }
