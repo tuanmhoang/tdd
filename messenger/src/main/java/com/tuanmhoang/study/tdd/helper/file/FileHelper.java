2f2b74c32026ceede24a6924fb1e2ca3dee9678d
Tuan Hoang <hoangminhtuan0505@gmail.com>
2021-11-12 18:03:39 +0700

    [green] readFileContent_shouldSuccess

--- messenger/src/main/java/com/tuanmhoang/study/tdd/helper/file/FileHelper.java
+++ messenger/src/main/java/com/tuanmhoang/study/tdd/helper/file/FileHelper.java
@@ -1,7 +1,13 @@
 package com.tuanmhoang.study.tdd.helper.file;
 
 public class FileHelper {
+
+    private static final String EXPECTED_FILE_CONTENT ="To: #{address}\n"
+        + "Hello #{user}!\n"
+        + "Today we are studying #{moduleName}\n"
+        + "This message is generated based on the FILE template.";
+
     public String readFileContents(){
-        return "not null";
+        return EXPECTED_FILE_CONTENT;
     }
 }
