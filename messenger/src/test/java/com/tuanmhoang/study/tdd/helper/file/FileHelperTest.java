5d94e2915436b4166340e56aef33ce784814e27e
Tuan Hoang <hoangminhtuan0505@gmail.com>
2021-11-12 18:02:30 +0700

    [red] readFileContent_shouldSuccess

--- messenger/src/test/java/com/tuanmhoang/study/tdd/helper/file/FileHelperTest.java
+++ messenger/src/test/java/com/tuanmhoang/study/tdd/helper/file/FileHelperTest.java
@@ -2,14 +2,21 @@ package com.tuanmhoang.study.tdd.helper.file;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class FileHelperTest {
     private FileHelper fileHelper = new FileHelper();
 
+    private static final String EXPECTED_FILE_CONTENT ="To: #{address}\n"
+        + "Hello #{user}!\n"
+        + "Today we are studying #{moduleName}\n"
+        + "This message is generated based on the FILE template.";
+
     @Test
     public void readFileContent_shouldSuccess(){
         String fileContents = fileHelper.readFileContents();
         assertNotNull(fileContents);
+        assertEquals(EXPECTED_FILE_CONTENT, fileContents);
     }
 }
