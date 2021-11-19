eaba41930301ed5954dd973d32a33215f1353dd9
Tuan Hoang <hoangminhtuan0505@gmail.com>
2021-11-19 17:04:26 +0700

    [red] generateMessage_withTemplateCli_success

--- messenger/src/test/java/com/tuanmhoang/study/tdd/template/TemplateEngineTest.java
+++ messenger/src/test/java/com/tuanmhoang/study/tdd/template/TemplateEngineTest.java
@@ -2,6 +2,7 @@ package com.tuanmhoang.study.tdd.template;
 
 import com.tuanmhoang.study.tdd.Client;
 import com.tuanmhoang.study.tdd.helper.CliParameterHelper;
+import com.tuanmhoang.study.tdd.helper.ParameterHelper;
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -11,11 +12,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 public class TemplateEngineTest {
     private TemplateEngine templateEngine = new TemplateEngine();
 
     private Template template;
+
+    private ParameterHelper parameterHelper;
 
     private Client client = new Client("sample@test.com");
 
@@ -31,10 +36,37 @@ public class TemplateEngineTest {
 
     @Test
     public void generateMessage_withTemplateCli_success() {
-        template = new Template(new CliParameterHelper(System.in));
+        // case 1
+        parameterHelper = mock(CliParameterHelper.class);
+        doReturn(mockParams("sample@test.com", "Tuan", "TDD"))
+            .when(parameterHelper).getParams();
+
+        template = new Template(parameterHelper);
+
         String msg = templateEngine.generateMessage(template, client);
         assertEquals(msg, EXPECTED_MSG_FROM_CONSOLE);
+
+        // case 2
+        doReturn(mockParams("sample@test.com", "Tuan2", "AWS"))
+            .when(parameterHelper).getParams();
+
+        template = new Template(parameterHelper);
+
+        msg = templateEngine.generateMessage(template, client);
+
+        String expectedMsg2 = "To: sample@test.com\n"
+            + "Hello Tuan2!\n"
+            + "Today we are studying AWS\n"
+            + "This message is generated based on the CONSOLE template.";
+        assertEquals(msg, expectedMsg2);
     }
 
+    private Map<String, String> mockParams(String addr, String user, String module) {
+        Map<String, String> mocked = new HashMap<>();
+        mocked.put("address", addr);
+        mocked.put("user", user);
+        mocked.put("moduleName", module);
+        return mocked;
+    }
 
 }
